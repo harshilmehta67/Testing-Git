164351ae4bfe2c49597143fd4c9e0a7286c0495c
Harshil <harshilmehta67@gmail.com>
2021-03-22 21:04:34 +0530

    Debugging

--- learning/helloworld.js
+++ learning/helloworld.js
@@ -1,7 +1,9 @@
 var message = 'Hello World';
-console.log(message);
+// console.log(message);
 // Dummy code
 var sum = 0;
 for (var i = 0; i < 5; i++) {
     sum += i;
 }
+
+['1','a','b'].forEach(x => console.log(x)) ;
